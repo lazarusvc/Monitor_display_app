47060f899ef194dd0130f68a2e761b9755d3b6ae
Lazarus118 <austin.lazarus@gmail.com>
2017-04-21 14:20:04 -0400

    21.04.2017

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import os.path as op
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 import flask_admin as admin
 from flask_admin.contrib import fileadmin
@@ -15,11 +14,71 @@ app = Flask(__name__, template_folder='templates', static_folder='files')
 app.config['SECRET_KEY'] = '123456790'
 
 ##################################################
-# Flask views
+# HOME
 ##################################################
 @app.route('/')
 def index():
-     return render_template('index_new.html')
+    # importing .jpg from files dir
+    # =======================================================
+    images = []    
+    for file in os.listdir("files"):
+        if file.endswith(".jpg"):
+            images.append(os.path.join("files", file))
+        else:
+            continue         
+
+    # image description
+    # =======================================================
+    txt_file = open("files/descriptions/description.txt").read().strip()
+
+    # company description
+    # ========================================================
+    company_txt_file = open("files/descriptions/company.txt").read().strip()
+
+    # company slogan description
+    # ========================================================
+    company_slogan_txt_file = open("files/descriptions/company_slogan.txt").read().strip()
+
+    return render_template('index_new.html', images=images, 
+                                             txt_file=txt_file,
+                                             company_txt_file=company_txt_file,
+                                             company_slogan_txt_file=company_slogan_txt_file)
+
+##################################################
+# ADD DESCRIPTION
+##################################################
+@app.route('/add', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        add = request.form['add']
+        txt_file = open("files/description.txt", "a")    
+        txt_file.write(add + "\n")
+        txt_file.close()
+    return redirect(url_for('fileadmin.edit'))   
+
+##################################################
+# COMPAMY DESCRIPTION
+##################################################
+@app.route('/company', methods=['POST'])
+def company():
+    if request.method == 'POST':
+        company = request.form['company']
+        txt_file = open("files/company.txt", "w")    
+        txt_file.write(company + "\n")
+        txt_file.close()
+    return redirect(url_for('fileadmin.edit'))
+
+##################################################
+# COMPAMY SLOGAN DESCRIPTION
+##################################################
+@app.route('/company_slogan', methods=['POST'])
+def company_slogan():
+    if request.method == 'POST':
+        company_slogan = request.form['company_slogan']
+        txt_file = open("files/company_slogan.txt", "w")    
+        txt_file.write(company_slogan + "\n")
+        txt_file.close()
+    return redirect(url_for('fileadmin.edit'))            
 
 
 if __name__ == '__main__':
